0caa9b087d2693a2d1be4b87d3006a9d0109877e
miguelokc <185961450+miguelokc@users.noreply.github.com>
2025-07-17 14:03:08 -0500

    created dialog tag

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const myLibrary = []
 
+const showBtn = document.getElementById("show-dialog");
+const dialog = document.getElementById("dialog");
+const submitBtn = document.getElementById("submit")
+
+showBtn.addEventListener ("click",() =>{
+    dialog.showModal();
+})
+
+submitBtn.addEventListener ("click" , ()=>{
+    
+    const author = document.getElementById("author-input").value;
+    const title = document.getElementById("book-input").value;
+    const pages = document.getElementById("pages-input").value;
+    const read = document.getElementById("read-input").checked;
+    addBookToLibrary(author,title,pages,read);
+    displayBooks();
+    
+
+})
+
 class Book{
     constructor(author,title,pages,read){
         this.author = author;
@@ -14,16 +34,19 @@ function addBookToLibrary(author,title,pages,read){
     myLibrary.push(instance);
 }
 
-addBookToLibrary("J.R.R. Tolkien", "The Hobbit", 295, "not read yet");
+
 
 function displayBooks(){
     const container = document.querySelector(".books");
+    container.innerHTML = "";
     myLibrary.forEach(Book => {
+        const btn = document.createElement('button');
         const square = document.createElement("div")
         square.textContent = `${Book.title} by ${Book.author}, ${Book.pages} pages,${Book.read}`;
         container.appendChild(square);
+        btn.innerText = "Read Status";
+        container.appendChild(btn)
     });
     
 }
 
-displayBooks();
